74601648faeee9cd0e8b8030109ca9f6e8c63e92
Hyeonji-Ahn <ahj2875@gmail.com>
2023-02-09 14:58:33 -0500

    modified:   app.vue 	modified:   package-lock.json 	modified:   package.json

--- app.vue
+++ app.vue
@@ -1,58 +1,101 @@
 <template>
 
-<div class="overflow-x-auto">
-  <table class="table w-full">
-    <!-- head -->
-    <thead>
-      <tr>
-        <th>Date</th>
-        <th>Status</th>
-        <th>Place</th>
-        <th>Description</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="(a,i) in 4" :key="i" class=" " :class="i==today ? 'active' : ''">
-        <td>{{dates[i]}}</td>
-        <td>
-          <div v-if="isGame[i] && i >= today" class="badge badge-primary">GAME</div>
-          <div v-else-if= "isGame[i]" class="badge badge-secondary">Past Game</div>
-          <div v-else-if="i>=today" class="badge">Practice</div>
-        </td>
-        <td>{{ places[i] }}</td>
-        <td>
-          <div class="collapse">
-            <input type="checkbox" /> 
-            <div class="collapse-title text-xl font-medium">
-              {{ collapseTitle[i] }}
-            </div>
-            <div class="collapse-content"> 
-              <p>{{ description[i] }}</p>
-            </div>
-          </div>
-        </td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+  
+  
+  <div class="btn-group btn-group-vertical"> 
+     <!-- somehow it isnt work as vertical -->
+    <button @click="gameSch = false" class="btn" :class="!gameSch ? ' btn btn-active' : 'btn'">Week</button>
+    <button @click="gameSch = true" class="btn" :class="gameSch ? 'btn-active' : 'btn'">Game</button>
+  </div>
+
+  <div v-if="gameSch === false" class="overflow-x-auto">
+      <table class="table">
+        <!-- head -->
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Status</th>
+            <th>Place</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(a,i) in dates" :key="i" class=" " :class="i==today ? 'active' : ''">
+            <td>{{dates[i]}}</td>
+            <td>
+              <div v-if="isGame.includes(i) && i >= today" class="badge badge-primary">GAME</div>
+              <div v-else-if= "isGame.includes(i) " class="badge badge-secondary">Past Game</div>
+              <div v-else-if="i>=today" class="badge">Practice</div>
+            </td>
+            <td>{{ places[i] }}</td>
+            <td>
+              <div class="collapse">
+                <input type="checkbox" /> 
+                <div class="collapse-title text-xl font-medium">
+                  {{ collapseTitle[i] }}
+                </div>
+                <div class="collapse-content"> 
+                  <p>{{ description[i] }}</p>
+                </div>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <div v-else class="overflow-x-auto">
+      <table class="table">
+        <!-- head -->
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Status</th>
+            <th>Place</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        
+        <tbody>
+          <tr v-for="a in isGame" :key="a" class=" " :class="a==today ? 'active' : ''">
+            <td>{{dates[a]}}</td>
+            <td>
+              <div v-if="a >= today" class="badge badge-primary">GAME</div>
+              <div v-else class="badge badge-secondary">Past Game</div>
+            </td>
+            <td>{{ places[a] }}</td>
+            <td>
+              <div class="collapse">
+                <input type="checkbox" /> 
+                <div class="collapse-title text-xl font-medium">
+                  {{ collapseTitle[a] }}
+                </div>
+                <div class="collapse-content"> 
+                  <p>{{ description[a] }}</p>
+                </div>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+
 </template>
 
 <script>
-
   export default{
+    
     data(){
       return {
         dates : ["2/6","2/7","2/8","2/9"],
-        isGame : [true,false,true,false],
+        isGame : [0,2],
         today : 2,
         places: ["Elkins", "PFAC", "Somewhere", "Overtherainbow"],
         collapseTitle:["A", "b","c","d"],
-        description:["some longer description","I am doing this at 2 am","I need to read the Road","Maybe I will just watch youtube lol"]
-      }
-    }
-  }
-  
-
+        description:["some longer description","I am doing this at 2 am","I need to read the Road","Maybe I will just watch youtube lol"],
+        gameSch : false,
+      };
+    },
+  };
 </script>
-
-
