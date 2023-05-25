b7d2477c0ea70a1faae9a44ee40464a2dd2f6db3
Hyeonji-Ahn <ahj2875@gmail.com>
2023-05-25 13:07:44 -0400

    modified:   app.vue

--- app.vue
+++ app.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="navbar bg-base-200">
-  <a class="btn btn-ghost normal-case text-xl">Asian American Study</a>
-</div>
-  <iframe class = "w-full" src="https://www.google.co.uk/maps/d/embed?mid=1qOQE9Y2BGCgIntROnZxCA3x8Bq9-88E&ehbc=2E312F" width="" height="480"></iframe>
+    <a class="btn btn-ghost normal-case text-xl">Asian American Study</a>
+  </div>
+  <iframe class = "h-full" src="https://www.google.co.uk/maps/d/embed?mid=1qOQE9Y2BGCgIntROnZxCA3x8Bq9-88E&ehbc=2E312F" width="" height="480"></iframe>
   <footer class="footer footer-center p-10 bg-base-200 text-base-content rounded">
   <div>
     <div class="grid grid-flow-col gap-4">
