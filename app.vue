961a3e385cda3fb2d79190ae9fd9b86230138165
Hyeonji-Ahn <ahj2875@gmail.com>
2023-05-25 13:18:45 -0400

    modified:   map height

--- app.vue
+++ app.vue
@@ -2,17 +2,19 @@
   <div class="navbar bg-base-200">
     <a class="btn btn-ghost normal-case text-xl">Asian American Study</a>
   </div>
-  <iframe class = "h-full" src="https://www.google.co.uk/maps/d/embed?mid=1qOQE9Y2BGCgIntROnZxCA3x8Bq9-88E&ehbc=2E312F" width="" height="480"></iframe>
+  <div class="flex flex-col">
+    <iframe class = "w-full h-screen" src="https://www.google.co.uk/maps/d/embed?mid=1qOQE9Y2BGCgIntROnZxCA3x8Bq9-88E&ehbc=2E312F"></iframe>
+  </div>
   <footer class="footer footer-center p-10 bg-base-200 text-base-content rounded">
-  <div>
-    <div class="grid grid-flow-col gap-4">
-      <p>Developer: Tiffany Ahn, Ryan Tran</p>
-    </div>
     <div>
-      <a class="link link-hover" href="https://github.com/orgs/AsianAmericanStoryMap/dashboard">Github</a>
-    </div>
-  </div> 
-</footer>
+      <div class="grid grid-flow-col gap-4">
+        <p>Developer: Tiffany Ahn, Ryan Tran</p>
+      </div>
+      <div>
+        <a class="link link-hover" href="https://github.com/orgs/AsianAmericanStoryMap/dashboard">Github</a>
+      </div>
+    </div> 
+  </footer>
 </template>
 
 <script>
